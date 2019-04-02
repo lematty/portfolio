92789cc5fbb10f8949de7ffe558103a5e641eed3
Chris LeMatty <clematty@gmail.com>
2019-04-02 23:42:43 +0200

    Fixed menu icon to form x when open

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 function toggleMenu() {
   let height = document.getElementById('menu').style.height;
-  height !== '100%' ? height = '100%' : height = '0%';
+  height !== '100%' ? height  = '100%' : height  = '0%';
   document.getElementById('menu').style.height = height;
+  const e = document.getElementById('hamburger');
+  e.classList.toggle("menuOpen")
 }

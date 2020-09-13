8711721aacbe65702f36ed857677f2f9afbb8bd7
Chris LeMatty <clematty@gmail.com>
2020-09-13 16:28:44 +0200

    remove deprecated async in tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { Page } from './models';
@@ -6,7 +6,7 @@ import { PagesComponent } from './pages/pages.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
